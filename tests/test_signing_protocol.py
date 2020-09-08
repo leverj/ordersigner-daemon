6e2e1769510060b2d6c2b8966d110cde9589a421
Phoenix Zerin <phx@phx.ph>
2020-09-08 15:35:10 +1200

    Made tests easier to read 👓

--- tests/test_signing_protocol.py
+++ tests/test_signing_protocol.py
@@ -28,43 +28,13 @@ class SigningProtocolTest(TestCase):
         self.order_signer.futures_sig = '0xb4dc0de'
 
         self.protocol.dataReceived(dumps({
+            # Stripping out all but the bare essentials to keep test readable.
+            # See tests for ``leverj_ordersigner`` project for real-world
+            # example of a valid future order.
             'type': 'futures',
-            'instrument': {
-                'id': '1',
-                'symbol': 'BTCDAI',
-                'name': 'BTC/DAI',
-                'status': 'active',
-                'tickSize': 1,
-                'quoteSignificantDigits': 0,
-                'baseSignificantDigits': 4,
-                'baseSymbol': 'BTC',
-                'quoteSymbol': 'DAI',
-                'maxLeverage': 100,
-                'topic': 'index_BTCUSD',
-                'quote': {
-                    'name': 'DAI',
-                    'address': '0xb0F776EB352738CF25710d92Fca2f4A5e2c24D3e',
-                    'symbol': 'DAI',
-                    'decimals': 18
-                }
-            },
-            'order': {
-                'accountId': '0xc21b183A8050D1988117B86408655ff974d021A0',
-                'originator': '0x10758C328A02B511d2a7B8f55459929Fa760411f',
-                'instrument': '1',
-                'price': 9380,
-                'quantity': 1,
-                'marginPerFraction': '191428571428571430000',
-                'side': 'buy',
-                'orderType': 'SLM',
-                'timestamp': 1595560081494041,
-                'quote': '0xb0F776EB352738CF25710d92Fca2f4A5e2c24D3e',
-                'isPostOnly': False,
-                'reduceOnly': False,
-                'clientOrderId': 1,
-                'triggerPrice': 9385
-            },
-            'signer': '0xae9edfad7fa29b0d6e14b045b5e74f3360e041545cf64236e81e616849072d65',
+            'instrument': {},
+            'order': {},
+            'signer': '0x1337',
         }).encode('utf-8') + self.protocol.delimiter)
 
         self.assertEqual(self.transport.value(), dumps({
@@ -79,29 +49,13 @@ class SigningProtocolTest(TestCase):
         self.order_signer.spot_sig = '0xb4dc0de'
 
         self.protocol.dataReceived(dumps({
+            # Stripping out all but the bare essentials to keep test readable.
+            # See tests for ``leverj_ordersigner`` project for real-world
+            # example of a valid spot order.
             'type': 'spot',
-            'instrument': {
-                'symbol': 'LEVETH',
-                'quote': {
-                    'address': '0x0000000000000000000000000000000000000000',
-                    'decimals': 18
-                },
-                'base': {
-                    'address': '0x167cdb1aC9979A6a694B368ED3D2bF9259Fa8282',
-                    'decimals': 9
-                }
-            },
-            'order': {
-                'accountId': '0x167cdb1aC9979A6a694B368ED3D2bF9259Fa8282',
-                'side': 'buy',
-                'quantity': 12.3343,
-                'price': 23.44322,
-                'orderType': 'LMT',
-                'instrument': 'LEVETH',
-                'timestamp': 12382173200872,
-                'expiryTime': 1238217320021122,
-            },
-            'signer': '0xb98ea45b6515cbd6a5c39108612b2cd5ae184d5eb0d72b21389a1fe6db01fe0d',
+            'instrument': {},
+            'order': {},
+            'signer': '0x1337',
         }).encode('utf-8') + self.protocol.delimiter)
 
         self.assertEqual(self.transport.value(), dumps({
